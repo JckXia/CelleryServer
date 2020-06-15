235f4e2eb0beaf41494ca72b61dd0dcd2b3cccda
Jenny Qiao <jenny.qiao@uwaterloo.ca>
2020-06-15 18:35:52 -0400

    Add cascade for delete and update when routine_products table has updated/deleted an entry

--- src/main/resources/db/migration/V1__Database_bulk_creation_20200606.sql
+++ src/main/resources/db/migration/V1__Database_bulk_creation_20200606.sql
@@ -64,8 +64,8 @@ CREATE TABLE IF NOT EXISTS `Images` (
 ALTER TABLE `routines`  ADD CONSTRAINT fk_routines_user FOREIGN KEY (`user_fk`) REFERENCES `users` (id);
 ALTER TABLE `routines` ADD CONSTRAINT fk_routines_log FOREIGN KEY (`log_fk`) REFERENCES `Logs` (id);
 
-ALTER TABLE `ROUTINE_PRODUCTS` ADD CONSTRAINT fk_routines_routine_products FOREIGN KEY (`id_routine`) REFERENCES `routines`(id);
-ALTER TABLE `ROUTINE_PRODUCTS` ADD CONSTRAINT fk_products_routine_products FOREIGN KEY (`id_product`) REFERENCES `Products`(id);
+ALTER TABLE `ROUTINE_PRODUCTS` ADD CONSTRAINT fk_routines_routine_products FOREIGN KEY (`id_routine`) REFERENCES `routines`(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `ROUTINE_PRODUCTS` ADD CONSTRAINT fk_products_routine_products FOREIGN KEY (`id_product`) REFERENCES `Products`(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE `Logs` ADD CONSTRAINT fk_log_time FOREIGN KEY (`time_object_fk`) references `TimeObject`(id);
 
