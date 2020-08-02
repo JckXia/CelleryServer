4ac43b9305924013e4c205b565bccb96d02f7cbf
JckXia <ys3xia@uwaterloo.ca>
2020-08-01 22:20:27 -0400

    Add jwt token verification end point

---
+++ src/main/java/com/cellery/api/backend/ui/model/request/UserVerificationModel.java
@@ -0,0 +1,13 @@
+package com.cellery.api.backend.ui.model.request;
+
+public class UserVerificationModel {
+    private String jwtToken;
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+}
---
+++ src/main/java/com/cellery/api/backend/ui/model/response/UserVerificationRespModel.java
@@ -0,0 +1,22 @@
+package com.cellery.api.backend.ui.model.response;
+
+public class UserVerificationRespModel {
+    private String userName;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
